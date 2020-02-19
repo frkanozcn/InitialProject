f4e2be5aad4e1b281880b59f9fa9fcd21d2ff58b
Furkan Özcan <frkanozcn@gmail.com>
2020-02-19 23:41:16 +0300

    Data type conversion tests

--- Tester.py
+++ Tester.py
@@ -1,2 +1,24 @@
-string = "Fatma & Furkan"
+string = "Fatma"
 print(string[-10:7])
+a = 'Fatma'
+b = "Furkan"
+c = """FatmaFurkan"""
+print(a)
+print(b)
+print(c)
+# Fatma'nın
+fatma = 'Fatma\'nın'
+print(fatma)
+
+i = 2
+i = i + 1
+print(len(str(i)))
+
+a = "3"
+print(float(a))
+
+while int(a) > 0:
+    integer = int(a)
+    integer -= 1
+    print(a)
+    a = str(integer)
